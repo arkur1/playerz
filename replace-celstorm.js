244e1075223d0ec93bc65d198f6621ff7d823565
arkur1 <43163065+arkur1@users.noreply.github.com>
2025-06-30 15:03:07 +0800

    Update replace-celstorm.js

--- replace-celstorm.js
+++ replace-celstorm.js
@@ -1,8 +1,8 @@
 (function () {
     const iframes = document.querySelectorAll('iframe');
     iframes?.forEach(iframe => {
-        if(iframe.src?.indexOf('short.ink') > -1) {
-            iframe.src = iframe.src.replace('short.ink', 'short.icu');
+        if(iframe.src?.indexOf('celstorm.biz.id') > -1) {
+            iframe.src = iframe.src.replace('celstorm.biz.id', 'celstorm.my.id');
         }
     });
 })();
